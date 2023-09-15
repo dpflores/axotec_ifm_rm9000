53dfb44b8d7ef138a777a5081e9a583d9297cbb2
Del Piero Flores <delpier22.flores@gmail.com>
2023-09-15 17:33:38 -0500

    u

--- can_rm.py
+++ can_rm.py
@@ -32,6 +32,7 @@ class CANRM():
     def activate_speed(self):
         self.node.sdo[0x3010][0x1].raw = 0x1 
         print("Speed activated")
+        print(self.node.sdo[0x3010][0x1].raw)
 
     def deactivate_speed(self):
         self.node.sdo[0x3010][0x1].raw = 0x0
